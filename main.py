00b9534305c29b2b9f117f7ceb6cb6c513308208
Abhishek Tupe <abhishek.tupe.pune@gmail.com>
2022-05-30 11:35:12 +0530

    readme commit

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-def add(a,b):
+def add(a, b):
     return a+b
 
-def sub(a,b):
+
+def sub(a, b):
     return a-b
 
-def mul(a,b):
+
+def mul(a, b):
     return a*b
 
-def div(a,b):
+
+def div(a, b):
     return a//b
 
-x,y = map(int, input().split())
+
+x, y = map(int, input().split())
+print(f"1.Add\n2.Subtract\n3.Multiply\n4.Divide")
